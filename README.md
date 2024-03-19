e9076ec35f06980a270c64deb25d9e31a9ef088f
abdelkarim smaili <95588995+abdelkarimsmaili97@users.noreply.github.com>
2024-03-19 01:03:37 -0700

    upload the basic files

---
+++ README.md
@@ -0,0 +1,16 @@
+Replication package for Deep Learning based Vulnerability Prediction study.
+
+Requirement:
+torch==1.4.0
+gensim==3.8.0
+numpy==1.16.3
+tqdm==4.36.1
+
+
+To run different training and testing see the scripts in `scripts/` and `real_data_scripts/` folders.
+
+## Quickstart
+
+1. Go to Vuld_SySe/representation_learning
+2. Run "python api_test.py"
+3. Our results can be find in result.txt
---
+++ Vuld_SySe/draper.sh
@@ -0,0 +1,7 @@
+
+
+python draper_main.py --train_file ../data/draper/juliet.json --num_epochs 20 --model_path draper5.bin --test_every_epoch
+python draper_main.py --train_file ../data/draper/juliet.json --num_epochs 20 --model_path draper5.bin --test_every_epoch
+python draper_main.py --train_file ../data/draper/juliet.json --num_epochs 20 --model_path draper5.bin --test_every_epoch
+python draper_main.py --train_file ../data/draper/juliet.json --num_epochs 20 --model_path draper5.bin --test_every_epoch
+python draper_main.py --train_file ../data/draper/juliet.json --num_epochs 20 --model_path draper5.bin --test_every_epoch
